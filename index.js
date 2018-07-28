a7b87831d618a769dfd9a3f26369f1056c98649d
varshith <rockingvarshith@gmail.com>
2018-07-28 16:50:37 +0530

    created index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if(username =="admin" && password == "admin"){
+		alert("Login Successful");
+	}else{
+		alert("Login Unsuccessful");
+	}
+	return false;
+}
+	
